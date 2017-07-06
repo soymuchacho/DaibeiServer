b6bced04e6538f976ac631044b06b5f44d62248a
soymuchacho <guqi_282@126.com>
2017-07-06 18:51:58 +0800

    logger

---
+++ base/logger.py
@@ -0,0 +1,17 @@
+import logging
+
+logger = logging.getLogger("DaibeiServer.custom")
+
+
+def log_write(level,args,*kw):
+    str = args % kw
+    if level == 'debug':
+        logger.debug(str)
+    elif level == 'info':
+        logger.info(str)
+    elif level == 'error':
+        logger.error(str)
+    elif level == 'warning':
+        logger.warning(str)
+    else:
+        logger.critical(str)
---
+++ base/user.py
@@ -0,0 +1,16 @@
+from django.core.cache import cache
+
+class User:
+    def __init__(self,userid,username,password,email,phone):
+        self.userid = userid
+        self.username = username
+        self.password = password
+        self.email = email
+        self.phone = phone
+        self.token = ""
+
+    def SetPassword(self,newpwd):
+        self.password = newpwd
+
+    def GetPassword(self):
+        self.password = newpwd 
